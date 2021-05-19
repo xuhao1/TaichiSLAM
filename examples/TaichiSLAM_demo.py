8c9b600466de2823971c81c891420284f63a6a9b
Xu Hao <xuhao3e8@gmail.com>
2021-05-19 23:20:48 +0800

    Refactor

--- examples/TaichiSLAM_demo.py
+++ examples/TaichiSLAM_demo.py
@@ -16,21 +16,10 @@ pub = None
 project_in_taichi = True
 disp_in_rviz = False
 
-def render_map_to_particles(pars, pos_, colors, num_particles_, level):
-    pos = pos_[0:num_particles_,:]
-    if not TEXTURE_ENABLED:
-        max_z = np.max(pos[:,2])
-        min_z = np.min(pos[:,2])
-        colors = cm.jet((pos[:,2] - min_z)/(max_z-min_z))
-    pars.set_particles(pos)
-    radius = np.ones(num_particles_)*(K**(level-1))*grid_scale
-    pars.set_particle_radii(radius)
-    pars.set_particle_colors(colors)
-
-def taichioctomap_pcl_callback(cur_trans, msg):
+def taichioctomap_pcl_callback(octomap, cur_trans, msg):
     if cur_trans is None:
         return
-    if TEXTURE_ENABLED:
+    if octomap.TEXTURE_ENABLED:
         xyz_array, rgb_array = pointcloud2_to_xyz_rgb_array(msg)
     else:
         xyz_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(msg)
@@ -39,27 +28,27 @@ def taichioctomap_pcl_callback(cur_trans, msg):
     _R, _T = transform_msg_to_numpy(cur_trans)
     if project_in_taichi:
         for i in range(3):
-            input_T[None][i] = _T[i]
+            octomap.input_T[None][i] = _T[i]
             for j in range(3):
-                input_R[None][i, j] = _R[i, j]
-        recast_pcl_to_map(xyz_array, rgb_array, len(xyz_array), False)
+                octomap.input_R[None][i, j] = _R[i, j]
+        octomap.recast_pcl_to_map(xyz_array, rgb_array, len(xyz_array), False)
     else:
         pts = []
         for pt in xyz_array:
             p = _R.dot(pt) + _T
             pts.append([p[0], p[1], p[2]])
         pts = np.array(pts)
-        recast_pcl_to_map(pts, rgb_array, len(pts), True)
+        octomap.recast_pcl_to_map(pts, rgb_array, len(pts), True)
 
     global level
     if disp_in_rviz:
-        pub_to_ros(pub, x.to_numpy(), color.to_numpy())
-    level, pos_ = handle_render(scene, gui, pars, level)
+        pub_to_ros(pub, octomap.x.to_numpy(), octomap.color.to_numpy())
+    level, pos_ = handle_render(octomap, scene, gui, pars, level)
 
 def pub_to_ros(pub, pos_, colors_):
-    get_voxel_to_particles(level)
+    octomap.get_voxel_to_particles(level)
     pcl = PointCloud()
-    if TEXTURE_ENABLED:
+    if octomap.TEXTURE_ENABLED:
         pts = np.concatenate((pos_, colors_.astype(float)/255.0), axis=1)
         pub.publish(point_cloud(pts, '/world', has_rgb=True))
     else:
@@ -91,11 +80,12 @@ if __name__ == '__main__':
     pars = tina.SimpleParticles()
     material = tina.Lamp()
     scene.add_object(pars, material)
-    scene.init_control(gui, radius=map_scale*0.7, theta=-1.57,center=(0, 0, 0), is_ortho=True)
-    Broot.deactivate_all()
+    octomap = Octomap()
+    scene.init_control(gui, radius=octomap.map_scale_xy*0.7, theta=-1.57,center=(0, 0, 0), is_ortho=True)
     
     if disp_in_rviz:
         rospy.init_node("TaichiOctomap", disable_signals=False)
         pub = rospy.Publisher('/pcl', PointCloud2, queue_size=10)
 
-    iteration_over_bag('/home/xuhao/data/voxblox/cow_and_lady_dataset.bag', taichioctomap_pcl_callback)
+    iteration_over_bag('/home/xuhao/data/voxblox/cow_and_lady_dataset.bag', 
+        lambda _1, _2: taichioctomap_pcl_callback(octomap, _1, _2))
--- taichi_slam/mapping/taichi_octomap.py
+++ taichi_slam/mapping/taichi_octomap.py
@@ -8,154 +8,163 @@ import tina
 import time
 from matplotlib import cm
 
-ti.init(arch=ti.cpu)
+@ti.data_oriented
+class Octomap:
+    K = 2
+    def __init__(self):
+        self.R = 8
+        self.Rz = 7
+        self.N = Octomap.K**self.R
+        self.Nz = Octomap.K**self.Rz
+        self.map_scale_xy = 20
+        self.map_scale_z = 10
+        self.grid_scale_xy = self.map_scale_xy/self.N
+        self.grid_scale_z = self.map_scale_z/self.Nz
+        self.max_num_particles = 100000
+        self.MIN_RECAST_THRES = 2
 
-RES = 1024
-K = 2
-R = 8
-Rz = 7
-N = K**R
-Nz = K**Rz
-map_scale = 20
-map_scale_z = 10
-grid_scale = map_scale/N
-grid_scale_z = map_scale_z/Nz
-max_num_particles = 100000
-MIN_RECAST_THRES = 2
-Broot = ti.root
-B = ti.root
-TEXTURE_ENABLED = False
+        self.TEXTURE_ENABLED = False
 
-for r in range(R):
-    if r < Rz:
-        B = B.pointer(ti.ijk, (K, K, K))
-    else:
-        B = B.pointer(ti.ijk, (K, K, 1))
+        self.initialize_fields()
+        self.construct_octo_tree()
 
-Croot = ti.root
-C = ti.root
-for r in range(R):
-    if r < Rz:
-        C = C.pointer(ti.ijk, (K, K, K))
-    else:
-        C = C.pointer(ti.ijk, (K, K, 1))
+    def initialize_fields(self):
+        self.num_export_particles = ti.field(dtype=ti.i32, shape=())
+        self.input_R = ti.Matrix.field(3, 3, dtype=ti.f32, shape=())
+        self.input_T = ti.Vector.field(3, dtype=ti.f32, shape=())
+        self.x = ti.Vector.field(3, dtype=ti.f32, shape=self.max_num_particles)
+        self.color = ti.Vector.field(3, dtype=ti.i32, shape=self.max_num_particles)
 
-num_export_particles = ti.field(dtype=ti.i32, shape=())
-
-input_R = ti.Matrix.field(3, 3, dtype=ti.f32, shape=())
-input_T = ti.Vector.field(3, dtype=ti.f32, shape=())
-grid_scale_field = ti.Vector.field(3, dtype=ti.f32, shape=())
-N_field = ti.Vector.field(3, dtype=ti.f32, shape=())
-grid_scale_field = ti.Vector([grid_scale, grid_scale, grid_scale_z])
-N_field = ti.Vector([N/2, N/2, Nz/2])
-
-x = ti.Vector.field(3, dtype=ti.f32, shape=max_num_particles)
-color = ti.Vector.field(3, dtype=ti.i32, shape=max_num_particles)
-
-#qt.parent is the deepest of bitmasked
-qt = ti.field(ti.i32)
-Cqt = ti.Vector.field(3, ti.i32)
-
-B.place(qt)
-C.place(Cqt)
-
-print(f'The map voxel is:[{N}x{N}x{Nz}] map scale:[{map_scale}mx{map_scale}mx{map_scale_z}m], grid scale [{grid_scale:3.3f}x{grid_scale:3.3f}x{grid_scale_z:3.3f}]')
+        self.grid_scale_ = ti.Vector([self.grid_scale_xy, self.grid_scale_xy, self.grid_scale_z])
+        self.map_scale_ = ti.Vector([self.map_scale_xy, self.map_scale_xy, self.map_scale_z])
+        self.NC_ = ti.Vector([self.N/2, self.N/2, self.Nz/2])
+        self.N_ = ti.Vector([self.N, self.N, self.N])
 
 
-@ti.kernel
-def get_voxel_to_particles(level: ti.template()):
-    # Number for level
-    num_export_particles[None] = 0
-    tree = ti.static(qt)
-    if ti.static(level) > 0:
-        tree = ti.static(qt.parent(level))
-    for i, j, k in tree:
-        if qt[i, j, k] > MIN_RECAST_THRES:
-            index = ti.atomic_add(num_export_particles[None], 1)
-            if num_export_particles[None] < max_num_particles:
-                x[index][0] = i*grid_scale - map_scale/2
-                x[index][1] = j*grid_scale - map_scale/2
-                x[index][2] = k*grid_scale_z  - map_scale_z/2
-                if TEXTURE_ENABLED:
-                    color[index] = Cqt[i, j, k]
-
-
-@ti.kernel
-def recast_pcl_to_map(xyz_array: ti.ext_arr(), rgb_array: ti.ext_arr(), n: ti.i32, no_project: ti.template()):
-    for index in range(n):
-        pt = ti.Vector([
-            xyz_array[index,0], 
-            xyz_array[index,1], 
-            xyz_array[index,2]])
-        if not ti.static(no_project):
-            pt = input_R@pt + input_T
+    def construct_octo_tree(self):
+        K = Octomap.K
+        B = ti.root
         
-        pt = pt / grid_scale_field + N_field
-        pt.cast(int)
+        for r in range(self.R):
+            if r < self.Rz:
+                B = B.pointer(ti.ijk, (K, K, K))
+            else:
+                B = B.pointer(ti.ijk, (K, K, 1))
+        self.B = B
+        C = ti.root
+        for r in range(self.R):
+            if r < self.Rz:
+                C = C.pointer(ti.ijk, (K, K, K))
+            else:
+                C = C.pointer(ti.ijk, (K, K, 1))
+        self.C = C
+        
+        #qt.parent is the deepest of bitmasked
+        self.qt = ti.field(ti.i32)
+        self.Cqt = ti.Vector.field(3, ti.i32)
 
-        for d in ti.ti.static(range(3)):
-            if ti.static(d == 2):
-                if pt[d] >= Nz:
-                    pt[d] = Nz - 1
-            if pt[d] >= N:
-                pt[d] = N - 1
-            if pt[d] < 0:
-                pt[0] = 0
+        self.B.place(self.qt)
+        self.C.place(self.Cqt)
+        
+        self.B.deactivate_all()
+        self.C.deactivate_all()
 
-        qt[pt] += 1
+        print(f'The map voxel is:[{self.N}x{self.N}x{self.Nz}]', end ="")
+        print(f'map scale:[{self.map_scale_xy}mx{self.map_scale_xy}mx{self.map_scale_z}m]', end ="")
+        print(f'grid scale [{self.grid_scale_xy:3.3f}x{self.grid_scale_xy:3.3f}x{self.grid_scale_z:3.3f}]')
 
-        if ti.static(TEXTURE_ENABLED):
+
+    @ti.kernel
+    def get_voxel_to_particles(self, level: ti.template()):
+        # Number for level
+        self.num_export_particles[None] = 0
+        tree = ti.static(self.qt)
+        if ti.static(level) > 0:
+            tree = ti.static(self.qt.parent(level))
+        for i, j, k in tree:
+            if self.qt[i, j, k] > self.MIN_RECAST_THRES:
+                index = ti.atomic_add(self.num_export_particles[None], 1)
+                if self.num_export_particles[None] < self.max_num_particles:
+                    for d in ti.ti.static(range(3)):
+                        self.x[index][d] = ti.static([i, j, k][d])*self.grid_scale_[d] - self.map_scale_[d]/2
+                        if self.TEXTURE_ENABLED:
+                            self.color[index] = self.Cqt[i, j, k]
+
+
+    @ti.kernel
+    def recast_pcl_to_map(self, xyz_array: ti.ext_arr(), rgb_array: ti.ext_arr(), n: ti.i32, no_project: ti.template()):
+        for index in range(n):
+            pt = ti.Vector([
+                xyz_array[index,0], 
+                xyz_array[index,1], 
+                xyz_array[index,2]])
+            if not ti.static(no_project):
+                pt = self.input_R@pt + self.input_T
+            
+            pt = pt / self.grid_scale_ + self.NC_
+            pt.cast(int)
+
             for d in ti.ti.static(range(3)):
-                Cqt[pt][d] = rgb_array[index, d]
+                if pt[d] >= self.N_[d]:
+                    pt[d] = self.N_[d] - 1
+                if pt[d] < 0:
+                    pt[d] = 0
 
-def render_map_to_particles(pars, pos_, colors, num_particles_, level):
-    if num_particles_ == 0:
-        return
-    pos = pos_[0:num_particles_,:]
-    if not TEXTURE_ENABLED:
-        max_z = np.max(pos[:,2])
-        min_z = np.min(pos[:,2])
-        colors = cm.jet((pos[:,2] - min_z)/(max_z-min_z))
-    pars.set_particles(pos)
-    radius = np.ones(num_particles_)*(K**(level-1))*grid_scale
-    pars.set_particle_radii(radius)
-    pars.set_particle_colors(colors)
+            self.qt[pt] += 1
+
+            if ti.static(self.TEXTURE_ENABLED):
+                for d in ti.ti.static(range(3)):
+                    self.Cqt[pt][d] = rgb_array[index, d]
+
+    def render_map_to_particles(self, pars, pos_, colors, num_particles_, level):
+        if num_particles_ == 0:
+            return
+        pos = pos_[0:num_particles_,:]
+        if not self.TEXTURE_ENABLED:
+            max_z = np.max(pos[:,2])
+            min_z = np.min(pos[:,2])
+            colors = cm.jet((pos[:,2] - min_z)/(max_z-min_z))
+        pars.set_particles(pos)
+        radius = np.ones(num_particles_)*(self.K**(level-1))*self.grid_scale_xy
+        pars.set_particle_radii(radius)
+        pars.set_particle_colors(colors)
 
 
-@ti.kernel
-def random_init_octo(pts: ti.template()):
-    for i in range(pts):
-        x_ = ti.random(dtype = int)%N
-        y_ = ti.random(dtype = int)%N
-        z_ = ti.random(dtype = int)%Nz
-        qt[x_, y_, z_] =  ti.random(dtype = int)%10
+    @ti.kernel
+    def random_init_octo(self, pts: ti.template()):
+        for i in range(pts):
+            x_ = ti.random(dtype = int)%self.N
+            y_ = ti.random(dtype = int)%self.N
+            z_ = ti.random(dtype = int)%self.Nz
+            self.qt[x_, y_, z_] =  ti.random(dtype = int)%10
 
-def handle_render(scene, gui, pars, level):
+def handle_render(octomap, scene, gui, pars, level, substeps = 3):
     for e in gui.get_events(ti.GUI.PRESS):
         if e.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]:
             exit()
         elif e.key == "-":
             level += 1
-            if level == R:
-                level = R - 1
+            if level == octomap.R:
+                level = octomap.R - 1
         elif e.key == "=":
             level -= 1
             if level < 0:
                 level = 0
-    get_voxel_to_particles(level)
-    pos_ = x.to_numpy()
-    color_ = color.to_numpy()
-    render_map_to_particles(pars, pos_, color_, num_export_particles[None], level)
+    octomap.get_voxel_to_particles(level)
+    pos_ = octomap.x.to_numpy()
+    color_ = octomap.color.to_numpy()
+    octomap.render_map_to_particles(pars, pos_, color_, octomap.num_export_particles[None], level)
 
-    scene.input(gui)
-    scene.render()
-    gui.set_image(scene.img)
-    gui.text(content=f'Level {level:.2f} num_particles {num_export_particles[None]} grid_scale {(K**(level))*grid_scale} incress =; decress -',
-            pos=(0, 0.8),
-            font_size=20,
-            color=0x080808)
+    for i in range(substeps):
+        scene.input(gui)
+        scene.render()
+        gui.set_image(scene.img)
+        gui.text(content=f'Level {level:.2f} num_particles {octomap.num_export_particles[None]} grid_scale {(octomap.K**(level))*octomap.grid_scale_xy} incress =; decress -',
+                pos=(0, 0.8),
+                font_size=20,
+                color=0x080808)
 
-    gui.show()
+        gui.show()
     return level, pos_
 
 if __name__ == '__main__':
@@ -167,11 +176,11 @@ if __name__ == '__main__':
     pars = tina.SimpleParticles()
     material = tina.Lamp()
     scene.add_object(pars, material)
-    scene.init_control(gui, radius=map_scale*2, theta=-1.0, center=(0, 0, 0), is_ortho=True)
-    Broot.deactivate_all()
+    octomap = Octomap()
+    scene.init_control(gui, radius=octomap.map_scale_xy*2, theta=-1.0, center=(0, 0, 0), is_ortho=True)
 
     #Level = 0 most detailed
-    random_init_octo(1000)
+    octomap.random_init_octo(1000)
 
     while gui.running:
-        level, _ = handle_render(scene, gui, pars, level)
+        level, _ = handle_render(octomap, scene, gui, pars, level)
