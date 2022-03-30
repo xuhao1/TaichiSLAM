Fix gradient culmaltive issue.

scripts/simple_gradient_descent_BA.py
@@ -5,18 +5,29 @@ sys.path.insert(0,os.path.dirname(__file__) + "/../")
 from taichi_slam.taichi_transformations import *
 from transformations import *
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from math import *
 
 ti.init()
 
 Np = 10 # Poses
 Nl = 100 # landmarks
 Nzmax = Np*Nl #observation
-max_iter = 100
-rate = 3e-3
+max_iter = 1000
+rate = 8e-5
 
-sigma_z = 3/385 # sigma of observation
-sigma_L = 0.3 # sigma of landmark positions
+sigma_z = 0./385 # sigma of observation
+sigma_L = 0.1 # sigma of landmark positions
 sigma_T = 0.1 #sigma of camera position
+sigma_Q = 0.02 #sigma of camera quaternion
+
+sqrt_inf_z = 1.
+
+max_camera_pos = np.array([1, 1, 0])
+min_camera_pos = np.array([-1, -1, 0])
+
+max_landmark_pos = np.array([1.5, 1.5, 2])
+min_landmark_pos = np.array([-1.5, -1.5, 0.5])
 
 q_poses = ti.Vector.field(n=4, dtype=ti.f32, shape=Np, needs_grad=True) #Quaternion of poses
 T_poses = ti.Vector.field(n=3, dtype=ti.f32, shape=Np, needs_grad=True) #Translation of poses
@@ -43,8 +54,7 @@ def reprojection_err(z_l, l_p, q, T):
     l_ = l_p - T
     l_b = QuaternionRotate(QuaternionInverse(q), l_)
     z = project_to_image(l_b)
-    e =  z - z_l
-    # print("reprojection_err", z, z_l, e[0]*e[0] + e[1] * e[1])
+    e =  (z - z_l)*sqrt_inf_z
     return e[0]*e[0] + e[1] * e[1]
 
 @ti.kernel
@@ -53,26 +63,29 @@ def func():
         ind_p = Z_pind[i]
         ind_l = Z_lind[i]
         err = reprojection_err(Z_l[i], L_p[ind_l], q_poses[ind_p], T_poses[ind_p])
-        loss[None] += err/Nz[None]
+        loss[None] += err
 
 def generate_poses():
     global Pgt, Qgt
     for i in range(Np):
-        pgt = [i/10. , 0., 0.]
+        pgt = np.array([i//sqrt(Np), i%sqrt(Np), 0.])
+        pgt = pgt/sqrt(Np) * (max_camera_pos - min_camera_pos) + min_camera_pos
+        qgt = [0., 0, 0, 1.]
+        Qgt.append(qgt) #Attenti
+        
         p = np.random.normal(pgt, sigma_T, size=(3))
-
         T_poses[i] = ti.Vector(p)
-        q_poses[i] = ti.Vector([0., 0.1, 0., 1.])
+        qnoise = np.random.normal(qgt, sigma_Q, size=4)
+        q_poses[i] = ti.Vector(qnoise)
         q_poses[i] = q_poses[i]/q_poses[i].norm()
 
         Pgt.append(pgt)
-        Qgt.append([0., 0, 0, 1.]) #Attenti
     Pgt = np.array(Pgt)
     Qgt = np.array(Qgt)
 
 def generate_landmarks():
     global Lgt
-    Lgt = np.random.uniform([-5, -5, 0.5], [5, 5, 10], size=(Nl, 3))
+    Lgt = np.random.uniform(min_landmark_pos, max_landmark_pos, size=(Nl, 3))
     Lnoise = Lgt + np.random.normal(0, sigma_L, size=(Nl, 3))
     for i in range(Nl):
         L_p[i] = ti.Vector(Lnoise[i])
@@ -80,63 +93,117 @@ def generate_landmarks():
 def generate_observations():
     c = 0
     for i in range(Nl):
+        c_l = 0
         for j in range(Np):
             Pl = Lgt[i]
             p = Pgt[j]
             q = Qgt[j]
-            q_inv = quaternion_inverse_(q)
-            Pl = quaternion_rotate_(q_inv, Pl - p)
+            q_inv = QuaternionInverse_(q)
+            Pl = QuaternionRotate_(q_inv, Pl - p)
             x = np.random.normal(Pl[0] / Pl[2], sigma_z)
             y = np.random.normal(Pl[1] / Pl[2], sigma_z)
-            if np.fabs(x) < 1 and np.fabs(y) < 1:
+            if np.fabs(x) < 2 and np.fabs(y) < 2 and Pl[2] > 0:
                 Z_l[c] = ti.Vector([x, y])
                 Z_lind[c] = i
                 Z_pind[c] = j
                 c += 1
+                c_l+=1
+        # print("Landmark", i, Lgt[i],  "observed by", c_l)
     Nz[None] = c
+    print("Observations", Nz[None])
+
+def visual_sparse_map(qs, Ts, Ls, Lgt, ax=None, title="SparseMap", axis_len = 0.5):
+    if ax is None:
+        fig = plt.figure("SparseMap", figsize=(15,10))
+        plt.tight_layout()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.grid(True)
+    ax.clear()
+    ax.set_title(title)
+    # ax.scatter(Ts[:,0], Ts[:,1], Ts[:,2], color="black",marker=".",label=f"Camera Poses")
+    ax.scatter(Ls[:,0], Ls[:,1], Ls[:,2], color="orange",marker="x",label=f"Landmark Est")
+    ax.scatter(Lgt[:,0], Lgt[:,1], Lgt[:,2], color="gray",marker="x",label=f"Landmark GT")
     
-@ti.func
+    range_x = np.max(Ls[:,0]) - np.min(Ls[:,0])
+    range_y = np.max(Ls[:,1]) - np.min(Ls[:,1])
+    range_z = np.max(Ls[:,2]) - np.min(Ls[:,2])
+    # ax.set_box_aspect((1, range_y/range_x, range_z/range_x))
+
+    x_axis = np.zeros(Ts.shape)
+    y_axis = np.zeros(Ts.shape)
+    z_axis = np.zeros(Ts.shape)
+    for i in range(len(qs)):
+        x_axis[i] = QuaternionRotate_(qs[i], [1., 0., 0.])*axis_len
+        y_axis[i] = QuaternionRotate_(qs[i], [0., 1., 0.])*axis_len
+        z_axis[i] = QuaternionRotate_(qs[i], [0., 0., 1.])*axis_len
+    ax.quiver(Ts[:,0], Ts[:,1], Ts[:,2], x_axis[:, 0], x_axis[:, 1], x_axis[:, 2], linewidths=-.5, arrow_length_ratio=0.,  color="red", label="x axes")
+    ax.quiver(Ts[:,0], Ts[:,1], Ts[:,2], y_axis[:, 0], y_axis[:, 1], y_axis[:, 2], linewidths=-.5, arrow_length_ratio=0.,  color="green", label="y axes")
+    ax.quiver(Ts[:,0], Ts[:,1], Ts[:,2], z_axis[:, 0], z_axis[:, 1], z_axis[:, 2], linewidths=-.5, arrow_length_ratio=0.,  color="blue", label="z axes")
+    ax.legend()
+
+    return ax
+    
+def visualize(loss_log, Qlog, Tlog, Llog, Lgt):
+    plt.figure("loss")
+    plt.title("loss")
+    ticks = range(len(loss_log))
+    plt.plot(ticks, loss_log)
+    plt.legend()
+    plt.grid()
+    plt.pause(0.5)
+
+    #Visuallize landmarks of poses
+    ax = None
+    Qlog = np.array(Qlog)
+    Tlog = np.array(Tlog)
+    Llog = np.array(Llog)
+    for i in range(0, len(Qlog), 10):
+        ax = visual_sparse_map(Qlog[i,:], Tlog[i,:], Llog[i,:], Lgt, ax,  title=f"TaichiSlam: SparseMap {i}")
+        plt.pause(0.02)
+    plt.show()
+
+@ti.kernel
 def gradient_descent():
     for i in range(Np):
         T_poses[i] -= rate*T_poses.grad[i]
         grad_tangent = q_poses.grad[i].transpose()@PlusQuaternionJacobian(q_poses[i])
-        # print("q", q_poses[i], q_poses.grad[i], grad_tangent)
         q_poses[i] = QuaternionRetraction(q_poses[i], -rate*grad_tangent.transpose()) #Need retraction
     for i in range(Nl):
         L_p[i] = L_p[i] - rate*L_p.grad[i]
 
-@ti.kernel
+
 def iteration():
+    ti.clear_all_gradients()
     loss[None] = 0
     loss.grad[None] = 1
     func()
     func.grad()
     gradient_descent()
 
-np.random.seed(0)
-generate_poses()
-generate_landmarks()
-generate_observations()
-
-loss.grad[None] = 1
-func()
-func.grad()
-print("Initial loss", loss[None])
-# print(q_poses.grad)
-# print("Gradient of T_poses:\n", T_poses.grad)
-# print("Gradient of L_p:\n", L_p.grad)
-
 loss_log = []
-for i in range(max_iter):
-    iteration()
-    func()
-    loss_log.append(loss[None])
-    # print("Iter", i, "loss", loss[None])
+Qlog = []
+Tlog = []
+Llog = []
 
-plt.figure("loss")
-plt.title("loss")
-ticks = range(len(loss_log))
-plt.plot(ticks, loss_log)
-plt.legend()
-plt.grid()
-plt.show()
+if __name__ == "__main__":
+    np.random.seed(0)
+    generate_poses()
+    generate_landmarks()
+    generate_observations()
+
+    loss.grad[None] = 1
+    func()
+    func.grad()
+    print("Initial loss", loss[None])
+    print("L_p.grad", L_p.grad.to_numpy()*1000)
+    # print("q_poses.grad", q_poses.grad)
+
+    for i in range(0, max_iter):
+        iteration()
+        loss_log.append(loss[None])
+        Llog.append(L_p.to_numpy())
+        Tlog.append(T_poses.to_numpy())
+        Qlog.append(q_poses.to_numpy())
+
+    visualize(loss_log, Qlog, Tlog, Llog, Lgt)
+    

taichi_slam/taichi_transformations.py
@@ -1,5 +1,7 @@
 import taichi as ti
 import numpy as np
+
+#The order of taichi vector is follow x y z w
 kW = 3
 kX = 0
 kY = 1
@@ -23,24 +25,6 @@ def QuaternionInverse(q):
 @ti.func
 def QuaternionRotate(q, v):
     R = QuaternionMatrix(q)
-    v = R@v
-    return v
-
-def quaternion_inverse_(q):
-    return [-q[kX], -q[kY], -q[kZ], q[kW]]
-
-def quaternion_matrix_(q):
-    # p.115@Quaternion kinematics for the error-state KF
-    qw = q[kW]
-    qx = q[kX]
-    qy = q[kY]
-    qz = q[kZ]
-    return np.array([[qw*qw+qx*qx-qy*qy-qz*qz, 2*(qx*qy-qw*qz), 2*(qx*qz+qw*qy)], 
-        [2*(qx*qy + qw*qz), (qw*qw - qx*qx + qy*qy - qz*qz), 2*(qy*qz-qw*qx)],
-        [2*(qx*qz-qw*qy), 2*(qy*qz+qw*qx), qw*qw-qx*qx-qy*qy + qz*qz]])
-
-def quaternion_rotate_(q, v):
-    R = quaternion_matrix_(q)
     v = R@v
     return v
 
@@ -80,3 +64,21 @@ def QuaternionRetraction(q, delta):
                         ti.cos(norm_delta)])
         qret = QuaternionMultiply(dq, q)
     return qret
+
+def QuaternionInverse_(q):
+    return [-q[kX], -q[kY], -q[kZ], q[kW]]
+
+def QuaternionMatrix_(q):
+    # p.115@Quaternion kinematics for the error-state KF
+    qw = q[kW]
+    qx = q[kX]
+    qy = q[kY]
+    qz = q[kZ]
+    return np.array([[qw*qw+qx*qx-qy*qy-qz*qz, 2*(qx*qy-qw*qz), 2*(qx*qz+qw*qy)], 
+        [2*(qx*qy + qw*qz), (qw*qw - qx*qx + qy*qy - qz*qz), 2*(qy*qz-qw*qx)],
+        [2*(qx*qz-qw*qy), 2*(qy*qz+qw*qx), qw*qw-qx*qx-qy*qy + qz*qz]])
+
+def QuaternionRotate_(q, v):
+    R = QuaternionMatrix_(q)
+    v = R@v
+    return v